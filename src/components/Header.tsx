db85e91b1d36e1123e976a6c0cc9b5b90590c44c
Maicon <devmaicon@gmail.com>
2022-05-24 21:27:55 -0300

    correção btn e link header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,29 +35,29 @@ export function Header() {
                         setIsOpen={setIsOpenModalPassword}
                         isOpen={isOpenModalPassword}
                     />
-                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 z-10">
+                    <div className="z-10 px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="sr-only">
                                         Open main menu
                                     </span>
                                     {open ? (
                                         <XIcon
-                                            className="block h-6 w-6"
+                                            className="block w-6 h-6"
                                             aria-hidden="true"
                                         />
                                     ) : (
                                         <MenuIcon
-                                            className="block h-6 w-6"
+                                            className="block w-6 h-6"
                                             aria-hidden="true"
                                         />
                                     )}
                                 </Disclosure.Button>
                             </div>
-                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex-shrink-0 flex items-center">
+                            <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                                <div className="flex items-center flex-shrink-0">
                                     <Image
                                         src={imageLogoUrl}
                                         width={40}
@@ -65,7 +65,7 @@ export function Header() {
                                         alt="logo tasks"
                                     />
                                 </div>
-                                <div className="hidden sm:block sm:ml-6  justify-center">
+                                <div className="justify-center hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4 justify-center mt-[2px]">
                                         {navigation.map((item) => (
                                             <Link
@@ -95,22 +95,22 @@ export function Header() {
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
                                     type="button"
-                                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                    className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                 >
                                     <span className="sr-only">
                                         View notifications
                                     </span>
                                     <BellIcon
-                                        className="h-6 w-6"
+                                        className="w-6 h-6"
                                         aria-hidden="true"
                                     />
                                 </button>
-                                <ThemeButton className="h-8 w-8 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" />
+                                <ThemeButton className="w-8 h-8 p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" />
 
                                 {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
+                                <Menu as="div" className="relative ml-3">
                                     <div>
-                                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                        <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                             <span className="sr-only">
                                                 Open user menu
                                             </span>
@@ -134,7 +134,7 @@ export function Header() {
                                         leaveFrom="transform opacity-100 scale-100"
                                         leaveTo="transform opacity-0 scale-95"
                                     >
-                                        <Menu.Items className="z-20 origin-top-right absolute right-0 mt-2  rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <Menu.Items className="absolute right-0 z-20 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                             <Menu.Item>
                                                 {({ active }) => (
                                                     <a
@@ -200,20 +200,19 @@ export function Header() {
                     <Disclosure.Panel className="sm:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1">
                             {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={`
+                                <Link href={item.href} passHref key={item.name}>
+                                    <a
+                                        className={`
                                         ${
                                             router.asPath === item.href
                                                 ? "bg-gray-900 text-white"
                                                 : "text-gray-300 hover:bg-gray-700 hover:text-white"
                                         }
                                         block px-3 py-2 rounded-md text-base font-medium`}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
+                                    >
+                                        {item.name}
+                                    </a>
+                                </Link>
                             ))}
                         </div>
                     </Disclosure.Panel>
